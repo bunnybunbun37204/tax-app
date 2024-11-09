8f39b7b841e9ebeb3593b0e9e87af26b67c1a371
Siwadol_23 <siwadolrungmart@gmail.com>
2024-11-09 17:05:04 +0700

    fix(check complete): check complete

--- src/components/elements/addIncome.tsx
+++ src/components/elements/addIncome.tsx
@@ -7,24 +7,24 @@ import {
   Folders,
   SquareDashed,
   Paperclip,
-} from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Label } from "@/components/ui/label";
-import { AlertSure } from "./alertSure";
-import { useState } from "react";
+} from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardFooter } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Label } from '@/components/ui/label';
+import { AlertSure } from './alertSure';
+import { useState } from 'react';
 
 export const AddIncomeForm = () => {
-  const [tab, setTab] = useState("input");
-  const [salary, setSalary] = useState("");
-  const [royalty, setRoyalty] = useState("");
-  const [investment, setInvestment] = useState("");
-  const [rent, setRent] = useState("");
-  const [freelance, setFreelance] = useState("");
-  const [contract, setContract] = useState("");
-  const [other, setOther] = useState("");
+  const [tab, setTab] = useState('input');
+  const [salary, setSalary] = useState('');
+  const [royalty, setRoyalty] = useState('');
+  const [investment, setInvestment] = useState('');
+  const [rent, setRent] = useState('');
+  const [freelance, setFreelance] = useState('');
+  const [contract, setContract] = useState('');
+  const [other, setOther] = useState('');
   const [openAlert, setOpenAlert] = useState(false);
 
   const [files, setFiles] = useState<{
@@ -62,12 +62,12 @@ export const AddIncomeForm = () => {
       parseOrZero(salary) * 0.5 > 100000
         ? 100000
         : parseOrZero(salary) + parseOrZero(royalty) * 0.5 > 100000
-        ? 100000
-        : parseOrZero(royalty) +
-          parseOrZero(rent) * 0.2 +
-          parseOrZero(freelance) * 0.45 +
-          parseOrZero(contract) * 0.6 +
-          parseOrZero(other) * 0.6;
+          ? 100000
+          : parseOrZero(royalty) +
+            parseOrZero(rent) * 0.2 +
+            parseOrZero(freelance) * 0.45 +
+            parseOrZero(contract) * 0.6 +
+            parseOrZero(other) * 0.6;
 
     const inputData = {
       Salary: salary,
@@ -90,15 +90,15 @@ export const AddIncomeForm = () => {
       OtherFile: files.otherFile?.name,
     };
 
-    localStorage.setItem("salaryAfterTax", `${sumSalary - calculated}`);
+    localStorage.setItem('salaryAfterTax', `${sumSalary - calculated}`);
 
     console.log(JSON.stringify(inputData, null, 2));
     console.log(JSON.stringify(inputDataFile, null, 2));
-    console.log(localStorage.getItem("salaryAfterTax"));
+    console.log(localStorage.getItem('salaryAfterTax'));
   };
   const handleFileChange = (
     event: React.ChangeEvent<HTMLInputElement>,
-    fileName: keyof typeof files
+    fileName: keyof typeof files,
   ) => {
     const file = event.target.files?.[0] || null;
     setFiles((prevFiles) => ({
@@ -118,23 +118,18 @@ export const AddIncomeForm = () => {
           <TabsTrigger
             value="input"
             className="text-black text-sm text-center font-normal font-notosansthai basis-1/2 h-8 px-3 py-1.5 rounded-[3px] data-[state=active]:bg-sakura"
-            onClick={() => onTabChange("input")}
-          >
+            onClick={() => onTabChange('input')}>
             กรอกรายได้
           </TabsTrigger>
           <TabsTrigger
             value="upload"
             className="text-black text-sm text-center font-normal font-notosansthai basis-1/2 h-8 px-3 py-1.5 rounded-[3px] data-[state=active]:bg-sakura"
-            onClick={() => onTabChange("upload")}
-          >
+            onClick={() => onTabChange('upload')}>
             อัพโหลดไฟล์รายได้
           </TabsTrigger>
         </TabsList>
         {/* input content */}
-        <TabsContent
-          value="input"
-          className="max-w-md w-full flex flex-col gap-4 mt-4"
-        >
+        <TabsContent value="input" className="max-w-md w-full flex flex-col gap-4 mt-4">
           <Card className="bg-transparent shadow-transparent border-transparent">
             <CardContent className="flex gap-4 flex-col p-0 mb-4">
               <div className="h-10 px-3 py-2 bg-white rounded-md border border-blood items-center gap-2 inline-flex">
@@ -212,26 +207,22 @@ export const AddIncomeForm = () => {
               <Button
                 onClick={() => setOpenAlert(true)}
                 disabled={
-                  salary === "" &&
-                  royalty === "" &&
-                  investment === "" &&
-                  rent === "" &&
-                  freelance === "" &&
-                  contract === "" &&
-                  other === ""
+                  salary === '' &&
+                  royalty === '' &&
+                  investment === '' &&
+                  rent === '' &&
+                  freelance === '' &&
+                  contract === '' &&
+                  other === ''
                 }
-                className="text-white h-10 text-sm font-medium font-notosansthai bg-blood rounded-md justify-center items-center inline-flex w-full hover:cursor-pointer hover:bg-sakura"
-              >
+                className="text-white h-10 text-sm font-medium font-notosansthai bg-blood rounded-md justify-center items-center inline-flex w-full hover:cursor-pointer hover:bg-sakura">
                 ยืนยันข้อมูล
               </Button>
             </CardFooter>
           </Card>
         </TabsContent>
         {/* upload content */}
-        <TabsContent
-          value="upload"
-          className="max-w-md w-full flex flex-col gap-4 mt-0"
-        >
+        <TabsContent value="upload" className="max-w-md w-full flex flex-col gap-4 mt-0">
           <Card className="bg-transparent shadow-transparent border-transparent">
             <CardContent className="flex gap-4 flex-col p-0 mb-4">
               <div className="h-10 px-3 py-2 bg-white rounded-md border border-blood items-center gap-2 inline-flex">
@@ -239,15 +230,12 @@ export const AddIncomeForm = () => {
                   id="salary"
                   type="file"
                   className="hidden"
-                  onChange={(event) => handleFileChange(event, "salaryFile")}
+                  onChange={(event) => handleFileChange(event, 'salaryFile')}
                 />
                 <Label
                   htmlFor="salary"
-                  className="text-blood text-sm font-normal font-notosansthai w-full hover:cursor-pointer"
-                >
-                  {files.salaryFile
-                    ? `File Added: ${files.salaryFile.name}`
-                    : "เงินเดือน โบนัส ค่าจ้าง"}
+                  className="text-blood text-sm font-normal font-notosansthai w-full hover:cursor-pointer">
+                  {files.salaryFile ? `File Added: ${files.salaryFile.name}` : 'เงินเดือน โบนัส ค่าจ้าง'}
                 </Label>
                 <Paperclip className="text-blood h-4 w-4" />
               </div>
@@ -256,15 +244,14 @@ export const AddIncomeForm = () => {
                   id="royalty"
                   type="file"
                   className="hidden"
-                  onChange={(event) => handleFileChange(event, "royaltyFile")}
+                  onChange={(event) => handleFileChange(event, 'royaltyFile')}
                 />
                 <Label
                   htmlFor="royalty"
-                  className="text-blood text-sm font-normal font-notosansthai w-full hover:cursor-pointer"
-                >
+                  className="text-blood text-sm font-normal font-notosansthai w-full hover:cursor-pointer">
                   {files.royaltyFile
                     ? `File Added: ${files.royaltyFile.name}`
-                    : "ค่าลิขสิทธิ์และสิทธิในทรัพย์สินทางปัญญา"}
+                    : 'ค่าลิขสิทธิ์และสิทธิในทรัพย์สินทางปัญญา'}
                 </Label>
                 <Paperclip className="text-blood h-4 w-4" />
               </div>
@@ -273,17 +260,14 @@ export const AddIncomeForm = () => {
                   id="investment"
                   type="file"
                   className="hidden"
-                  onChange={(event) =>
-                    handleFileChange(event, "investmentFile")
-                  }
+                  onChange={(event) => handleFileChange(event, 'investmentFile')}
                 />
                 <Label
                   htmlFor="investment"
-                  className="text-blood text-sm font-normal font-notosansthai w-full hover:cursor-pointer"
-                >
+                  className="text-blood text-sm font-normal font-notosansthai w-full hover:cursor-pointer">
                   {files.investmentFile
                     ? `File Added: ${files.investmentFile.name}`
-                    : "ดอกเบี้ย ปันผล กำไรจากCrypto"}
+                    : 'ดอกเบี้ย ปันผล กำไรจากCrypto'}
                 </Label>
                 <Paperclip className="text-blood h-4 w-4" />
               </div>
@@ -292,15 +276,12 @@ export const AddIncomeForm = () => {
                   id="rent"
                   type="file"
                   className="hidden"
-                  onChange={(event) => handleFileChange(event, "rentFile")}
+                  onChange={(event) => handleFileChange(event, 'rentFile')}
                 />
                 <Label
                   htmlFor="rent"
-                  className="text-blood text-sm font-normal font-notosansthai w-full hover:cursor-pointer"
-                >
-                  {files.rentFile
-                    ? `File Added: ${files.rentFile.name}`
-                    : "ค่าเช่า"}
+                  className="text-blood text-sm font-normal font-notosansthai w-full hover:cursor-pointer">
+                  {files.rentFile ? `File Added: ${files.rentFile.name}` : 'ค่าเช่า'}
                 </Label>
                 <Paperclip className="text-blood h-4 w-4" />
               </div>
@@ -309,15 +290,12 @@ export const AddIncomeForm = () => {
                   id="freelance"
                   type="file"
                   className="hidden"
-                  onChange={(event) => handleFileChange(event, "freelanceFile")}
+                  onChange={(event) => handleFileChange(event, 'freelanceFile')}
                 />
                 <Label
                   htmlFor="freelance"
-                  className="text-blood text-sm font-normal font-notosansthai w-full hover:cursor-pointer"
-                >
-                  {files.freelanceFile
-                    ? `File Added: ${files.freelanceFile.name}`
-                    : "ฟรีแลนซ์"}
+                  className="text-blood text-sm font-normal font-notosansthai w-full hover:cursor-pointer">
+                  {files.freelanceFile ? `File Added: ${files.freelanceFile.name}` : 'ฟรีแลนซ์'}
                 </Label>
                 <Paperclip className="text-blood h-4 w-4" />
               </div>
@@ -326,16 +304,15 @@ export const AddIncomeForm = () => {
                   id="contract"
                   type="file"
                   className="hidden"
-                  onChange={(event) => handleFileChange(event, "contractFile")}
+                  onChange={(event) => handleFileChange(event, 'contractFile')}
                 />
                 <Label
                   htmlFor="contract"
-                  className="text-blood text-sm font-normal font-notosansthai w-full hover:cursor-pointer"
-                >
+                  className="text-blood text-sm font-normal font-notosansthai w-full hover:cursor-pointer">
                   {files.contractFile
                     ? `File Added: ${files.contractFile.name}`
-                    : "ค่ารับเหมาทั้งค่าแรงและค่าของ"}
-                </Label>{" "}
+                    : 'ค่ารับเหมาทั้งค่าแรงและค่าของ'}
+                </Label>{' '}
                 <Paperclip className="text-blood h-4 w-4" />
               </div>
               <div className="h-10 px-3 py-2 bg-white rounded-md border border-blood items-center gap-2 inline-flex">
@@ -343,16 +320,13 @@ export const AddIncomeForm = () => {
                   id="other"
                   type="file"
                   className="hidden"
-                  onChange={(event) => handleFileChange(event, "otherFile")}
+                  onChange={(event) => handleFileChange(event, 'otherFile')}
                 />
                 <Label
                   htmlFor="other"
-                  className="text-blood text-sm font-normal font-notosansthai w-full hover:cursor-pointer"
-                >
-                  {files.otherFile
-                    ? `File Added: ${files.otherFile.name}`
-                    : "อื่น ๆ"}
-                </Label>{" "}
+                  className="text-blood text-sm font-normal font-notosansthai w-full hover:cursor-pointer">
+                  {files.otherFile ? `File Added: ${files.otherFile.name}` : 'อื่น ๆ'}
+                </Label>{' '}
                 <Paperclip className="text-blood h-4 w-4" />
               </div>
             </CardContent>
@@ -368,8 +342,7 @@ export const AddIncomeForm = () => {
                   files.contractFile === null &&
                   files.otherFile === null
                 }
-                className="text-white h-10 text-sm font-medium font-notosansthai bg-blood rounded-md justify-center items-center inline-flex w-full hover:cursor-pointer hover:bg-sakura"
-              >
+                className="text-white h-10 text-sm font-medium font-notosansthai bg-blood rounded-md justify-center items-center inline-flex w-full hover:cursor-pointer hover:bg-sakura">
                 ยืนยันข้อมูล
               </Button>
             </CardFooter>
