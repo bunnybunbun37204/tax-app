efa923f60e751539863e9597efe54e50d99cd697
bunyawatapp37204 <stonksfarm37204@gmail.com>
2024-11-11 16:16:46 +0700

    fix: salary

--- src/components/elements/addIncome.tsx
+++ src/components/elements/addIncome.tsx
@@ -91,7 +91,6 @@ export const AddIncomeForm = () => {
     localStorage.setItem('salaryAfterCal', `${sumSalary - calculated}`);
     localStorage.setItem('salaryAfterCal', `${sumSalary}`);
 
-
     console.log(JSON.stringify(inputData, null, 2));
     console.log(JSON.stringify(inputDataFile, null, 2));
     console.log(localStorage.getItem('salaryAfterCal'));
